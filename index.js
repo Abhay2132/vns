2207a4f0a80f2839a5abff01c117538369840583
abhay2132 <abhaybro768@gmail.com>
2022-09-03 20:16:07 +0530

    added engines in package.json

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-console.clear();
+// console.clear();
+(function() {
+	const http = require("http");
+	const port = process.env.PORT || 8080;
+	const router = require("./bin/router");
 
-const http = require("http");
-const port = process.env.PORT || 8080;
-const router = require("./bin/router");
+	http.createServer(router)
+		.listen(port, () => console.log("Server is online at localhost:%i", port));
+})()
 
-http.createServer(router)
-.listen(port, () => console.log("Server is online at localhost:%i \n", port));
+module.exports = () => console.log();
